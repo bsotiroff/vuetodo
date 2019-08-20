f81112fc416c03f6272e1ba1b1f3dbbfcd0cbf5d
Brandon Sotiroff <bsotiroff1@gmail.com>
2019-08-19 19:55:54 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DinoCard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DinoCard from './components/DinoCard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+    DinoCard,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/DinoCard.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="DinoCard">
+<header>{{title}}</header>
+<div>
+<input id="itemForm" v-on:keypress.enter="addItem"/>
+<button v-on:click="addItem">Add Dinosaur</button>
+</div>
+<ul>
+<li v-for="(item, index) in items">
+<button v-on:click="deleteItem(index)">X</button>
+{{item.text}}
+</li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DinoCard',
+  data () {
+    return {
+    title: "Dinosaurs",
+    items: [
+      {text: "stegasaurus", update: false},
+      {text: "velociraptor", update: false},
+      {text: "T Rex", update: false}
+    ]
+    }
+  },
+  methods: {
+    addItem: function(){
+      let input = document.getElementById('itemForm');
+
+      if(input.value !== ''){
+        this.items.push({
+          text: input.value
+        })
+      }
+      input.value = "";
+    },
+    deleteItem: function(index){
+      
+      this.items.splice(index,1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
